409159e16fdc16d631e268a491f9c468c2fba535
Berkan Unal <Berkanunal@gmail.com>
2020-03-18 21:14:14 +0300

    Add present tense for verbs

--- src/verbs.ts
+++ src/verbs.ts
@@ -1,22 +1,68 @@
 const verbs = [
-  "be",
-  "change",
-  "come",
-  "do",
-  "get",
-  "give",
-  "go",
-  "have",
-  "keep",
-  "let",
-  "make",
-  "put",
-  "say",
-  "see",
-  "seem",
-  "send",
-  "take",
-  "use"
+  {
+    word: "change",
+    presentTense: "changes",
+  },
+  {
+    word: "come",
+    presentTense: "comes",
+  },
+  {
+    word: "do",
+    presentTense: "does",
+  },
+  {
+    word: "get",
+    presentTense: "gets",
+  },
+  {
+    word: "give",
+    presentTense: "gives",
+  },
+  {
+    word: "go",
+    presentTense: "goes",
+  },
+  {
+    word: "keep",
+    presentTense: "keeps",
+  },
+  {
+    word: "let",
+    presentTense: "lets",
+  },
+  {
+    word: "look",
+    presentTense: "looks",
+  },
+  {
+    word: "make",
+    presentTense: "makes",
+  },
+  {
+    word: "put",
+    presentTense: "puts",
+  },
+  {
+    word: "say",
+    presentTense: "says",
+  },
+  {
+    word: "see",
+    presentTense: "sees",
+  },
+  {
+    word: "send",
+    presentTense: "sends",
+  },
+  {
+    word: "take",
+    presentTense: "takes",
+  },
+  {
+    word: "use",
+    presentTense: "uses",
+  },
 ];
 
-export default verbs;
+export default verbs.map(verb => verb.presentTense);
